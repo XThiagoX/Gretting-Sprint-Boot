0f5d9eeb1d89bad8747079bad9f5351a42da35cc
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-03-04 16:58:48 -0300

    <feat>[/greeting]: Alteração de Anotação de entrada de parametros, para pathVariable

--- src/main/kotlin/br/com/hello/rest_spring/GreetingController.kt
+++ src/main/kotlin/br/com/hello/rest_spring/GreetingController.kt
@@ -6,6 +6,7 @@ import org.springframework.http.HttpEntity
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
@@ -19,15 +20,17 @@ class GreetingController {
 
     private val counter = AtomicLong()
 
-    @GetMapping("/greeting")
+    @GetMapping( value = ["/greeting/{name}/{lastName}"])
     fun greeting(
-        @RequestParam(value = "Thiago", defaultValue = "Word") value: String
+        @PathVariable(value = "name") name: String?,
+        @PathVariable(value = "lastName") lastName: String?
     ): HttpEntity<Greeting> {
 
-        val greeting = Greeting(template.format(value))
-        val controllerMethod = methodOn(GreetingController::class.java).greeting(value)
+        val greeting = Greeting(name, lastName)
+        val controllerMethod = methodOn(GreetingController::class.java).greeting(name, lastName)
         greeting.add(linkTo<GreetingController> { }.withSelfRel())
 
         return ResponseEntity(greeting, HttpStatus.OK)
     }
+
 }
